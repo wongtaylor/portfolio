ef78025371fc57e4052c652a1d4b456bf6606a1c
Taylor Wong <wong.taylor97@gmail.com>
2018-06-27 09:22:41 -0700

    downloaded PHPMailer Pakcage

--- contact.php
+++ contact.php
@@ -1,5 +1,11 @@
 <?php 
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
 require 'PHPMailer-master/PHPMailerAutoload.php';
 
 /* Configure basic variables */
