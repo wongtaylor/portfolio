7fc6c010335627212d59eeeb3599feebdea4f30d
Taylor Wong <wong.taylor97@gmail.com>
2018-06-26 17:14:44 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,8 +1,14 @@
 <?php 
 
+require 'PHPMailer-master/PHPMailerAutoload.php';
+
 /* Configure basic variables */
-$from = 'Contact Form <wong.taylor97@gmail.com>';
-$sendTo = 'Contact Form <taylorwong@sandiego.edu>';
+$fromEmail = 'JohnDoe@gmail.com';
+$fromName = 'John Doe';
+
+$sendToEmail = 'taylorwong@sandiego.edu';
+$sendToName = 'Taylor Wong';
+
 $subject = 'New message from Portfolio Contact Form';
 
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');
@@ -12,20 +18,23 @@ $okMessage = 'Message was sent successfully. Thank you, I will get back to you s
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 
-
 /* Send message */
 error_reporting(E_ALL & ~E_NOTICE);
 
 try {
 	if(count($_POST) == 0) throw new \Exception('Form is empty');
-	$emailText = "You have a new message from your contact form\n=================================\n";
+	$emailTextHTML = "<h1>You have a new message from your contact form</h1><hr>";
+	$emailTextHTML .= "<table>";
 
 	foreach ($_POST as $key => $value) {
 		// if item in $_POST array already exists in $fields array, include in message 
 		if(isset($fields[$key])){
-			$emailText .= "$fields[$key]: $value\n";
+			$emailTextHTML .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
 		}
 	}
+
+	$emailTextHtml .= "</table><hr>";
+	$emailTextHtml .= "<p>Have a wonderful day,<br>Best,<br>Taylor</p>";
 
 	// add headers to email
 	$headers = array('Content-Type: text/plain; charset="UTF-8;"',
@@ -34,8 +43,22 @@ try {
 		'Return-Path: ' . $from,
 	);
 
-	// send email via php internal mail() function 
-	mail($sendTo, $subject, implode("\n", $headers));
+
+	// send email using PHPMailer
+	$mail = new PHPMailer;
+
+	$mail->setFrom($fromEmail, $fromName);
+	$mail->addAddress($sendToEmail, $sendToName); 
+	$mail->addReplyTo($from);
+
+	$mail->isHTML(true);
+
+	$mail->Subject = $subject;
+	$mail->msgHTML($emailTextHtml); 
+
+	if(!$mail->send()) {
+    	throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+	}
 
 	// $responseArray will be sent as JSON response to contact.html
 	$responseArray = array('type' => 'success', 'message' => $okMessage);
